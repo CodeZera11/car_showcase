bb7101ea7e64fc32c0c6a38799c39e7b7416ab2c
Bhavesh Yadav <codezera3@gmail.com>
2023-07-11 18:19:13 +0530

    added view more dialog box

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -2,10 +2,15 @@
 import { CustomButtonProps } from "@/types"
 import Image from "next/image"
 
-const CustomButton = ({ title, handleClick, containerStyles, btnType }: CustomButtonProps) => {
+const CustomButton = ({ title, handleClick, containerStyles, btnType, textStyles, rightIcon }: CustomButtonProps) => {
   return (
     <button className={`custom-btn ${containerStyles}`} type={ btnType || "button"} disabled={false} onClick={handleClick}>
-        <span className={`flex-1`}>{title}</span>
+        <span className={`flex-1 ${textStyles}`}>{title}</span>
+          {rightIcon && 
+            <div className="relative w-6 h-6">
+              <Image src={rightIcon} alt="right" fill className="object-contain" />
+            </div>
+          }
     </button>
   )
 }
